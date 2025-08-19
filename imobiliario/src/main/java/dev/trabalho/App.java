10c7aa849e7e1663796cd42ba5c69fc5d17edf82
Felipe Pilz <felipevilvertpilz@gmail.com>
2025-08-18 21:10:11 -0300

    estrutura

---
+++ imobiliario/src/main/java/dev/trabalho/App.java
@@ -0,0 +1,13 @@
+package dev.trabalho;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
